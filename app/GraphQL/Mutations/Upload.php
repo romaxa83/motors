e3db70e42465e38adaac8418001c95637ca8498d
roman <romaxa83@ukr.net>
2021-03-09 21:46:57 +0200

    upload file

--- app/GraphQL/Mutations/Upload.php
+++ app/GraphQL/Mutations/Upload.php
@@ -1,1 +1,28 @@
 <?php
+
+namespace App\GraphQL\Mutations;
+
+use App\Models\Image;
+
+class Upload
+{
+    /**
+     * @param  null  $_
+     * @param  array<string, mixed>  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        /** @var \Illuminate\Http\UploadedFile $file */
+        $file = $args['file'];
+
+        \Storage::put(Image::getPathOfImage(), $file);
+
+        $i = new Image();
+        $i->name = $file->getFilename();
+        $i->hash = $file->hashName();
+
+        $i->save();
+
+        return $i;
+    }
+}
--- app/Models/Image.php
+++ app/Models/Image.php
@@ -1,1 +1,46 @@
 <?php
+
+namespace App\Models;
+
+use App\Models\User\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $hash
+ * @property int $user_id
+ * @mixin \Eloquent
+ */
+
+class Image extends Model
+{
+    protected $table = 'images';
+
+    public static function getPathOfImage(string $filename = null, string $user_id = null): string
+    {
+        $patch = null;
+        if (null !== $user_id) {
+            $patch = $user_id . '/';
+        }
+
+        return 'uploads/' . $patch . $filename;
+    }
+
+    /**
+     * @example http://localhost/uploads/1/saldfhaskjdhflasd.jpg
+     *
+     * @return string
+     */
+    public function getUrlAttribute(): string
+    {
+        return \Asset($this->getPathOfImage($this->hash, $this->user_id));
+    }
+
+    public function user(): HasOne
+    {
+        return$this->hasOne(User::class, 'id', 'user_id');
+    }
+}
+
